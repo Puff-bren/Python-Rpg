d11b969940950bd7ff2d3a20671719d4766b6bf6
Puff-bren <brenlee1829@gmail.com>
2023-03-12 23:08:55 -0700

    my first git commit
    RPG with python

---
+++ main.py.py
@@ -0,0 +1,12 @@
+print("\t---------------------")
+print("\tWELCOME TO PYTHON RPG")  # Title & Spacers
+print("\t---------------------\n")
+# Classes
+print("CHOOSE YOUR CLASS: "
+      "\n______________________________________________________________________________________________________________"
+      "__________________________")
+print("KNIGHT- Resistance/Strength\t\t\t MAGE- Mp/Intelligence\t\t\t HUNTER- Dexterity/Recovery\t\t\t BARBARIAN- "
+      "Resistance/Strength")
+# knight
+print("\nKnights are adaptive with a\ngreat knowledge in defensive\nand offensive attacks. Knights\nwill have swords "
+      "and shields as\ntheir greatest choice in battle.")
